379e193d37d9a949c5958daa18927d5fb26799d1
Mohit Gupta <mohit.gpta28@gmail.com>
2020-04-11 17:53:23 -0500

    added spinner and users

---
+++ Documents/react-git-finder/src/App.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
+import './App.css';
+
+
+class App extends  Component{
+  state = {
+    users:[],
+    loading: false
+  }
+  async componentDidMount(){
+      this.setState({loading:true});
+      const response = await axios.get('https://api.github.com/users')
+      this.setState({users: response.data, loading: false})
+  }
+
+  render(){
+    return (
+      <div className="App">
+          <Navbar/>
+          <div className="container">
+              <Users loading={this.state.loading} users={this.state.users}/>
+          </div>
+      </div>
+    );
+  }
+ 
+}
+export default App;
